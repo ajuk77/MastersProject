0681273b9bb6ebd7339e13ae1219cbc3e2f61a92
ajinkya.kulkarni77@gmail.com <ajinkya.kulkarni77@gmail.com>
2019-04-19 14:11:53 -0700

    Enabled CORS at controller layer

--- access-id-server/src/main/java/com/csuf/cs/accessid/accessidserver/controller/AuthController.java
+++ access-id-server/src/main/java/com/csuf/cs/accessid/accessidserver/controller/AuthController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import com.csuf.cs.accessid.accessidserver.service.IAuthService;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
 	@Autowired
--- access-id-server/src/main/java/com/csuf/cs/accessid/accessidserver/controller/EmployeeController.java
+++ access-id-server/src/main/java/com/csuf/cs/accessid/accessidserver/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import com.csuf.cs.accessid.accessidserver.util.PasswordUtil;
 
 @RestController
 @RequestMapping("/api/employee")
+@CrossOrigin(origins = "*")
 public class EmployeeController {
 
 	@Autowired
