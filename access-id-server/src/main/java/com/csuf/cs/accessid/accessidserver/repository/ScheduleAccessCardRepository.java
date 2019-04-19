d38f60aaf95d32922aeb5a7cd78a0c88ec8f44a8
ajinkya.kulkarni77@gmail.com <ajinkya.kulkarni77@gmail.com>
2019-04-19 14:09:38 -0700

    Added repository layer for access card scheduling and visitors

---
+++ access-id-server/src/main/java/com/csuf/cs/accessid/accessidserver/repository/ScheduleAccessCardRepository.java
@@ -0,0 +1,9 @@
+package com.csuf.cs.accessid.accessidserver.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.csuf.cs.accessid.accessidserver.model.ScheduleAccessCard;
+
+public interface ScheduleAccessCardRepository extends CrudRepository<ScheduleAccessCard, Integer>{
+
+}
---
+++ access-id-server/src/main/java/com/csuf/cs/accessid/accessidserver/repository/VisitorRepository.java
@@ -0,0 +1,9 @@
+package com.csuf.cs.accessid.accessidserver.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.csuf.cs.accessid.accessidserver.model.Visitor;
+
+public interface VisitorRepository extends CrudRepository<Visitor, Integer>{
+
+}
