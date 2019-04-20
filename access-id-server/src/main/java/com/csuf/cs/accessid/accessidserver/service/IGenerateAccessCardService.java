9f67fd1cbebdacbaa7857f4a82be696361c61f92
ajinkya.kulkarni77@gmail.com <ajinkya.kulkarni77@gmail.com>
2019-04-19 17:54:12 -0700

    Added logic to generate access card id, need to add validations

---
+++ access-id-server/src/main/java/com/csuf/cs/accessid/accessidserver/service/IGenerateAccessCardService.java
@@ -0,0 +1,12 @@
+package com.csuf.cs.accessid.accessidserver.service;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface IGenerateAccessCardService {
+	
+	public Map<String, Object> generateAccessCard(Map<String, Object> payload);
+
+}
